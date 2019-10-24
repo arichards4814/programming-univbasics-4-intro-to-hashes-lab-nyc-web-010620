a2c2a19f03fb02378a1528a22309ee8c76c4cb0e
Andrew Richards <arichards4814@gmail.com>
2019-10-24 17:50:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,6 +27,9 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
+  newOne = {
+    :key => value
+  }
 end
 
 def read_from_hash(hash, key)
